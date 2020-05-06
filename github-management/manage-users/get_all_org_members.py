16863e9b3d8bf9f75d42aee55f54af2153b85fd9
Farkhod <sadykovfarkhod@gmail.com>
2020-05-06 13:55:18 -0500

    Updated all comments and ready to merge

--- github-management/manage-users/get_all_org_members.py
+++ github-management/manage-users/get_all_org_members.py
@@ -7,13 +7,15 @@ import sys
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
-g = Github(os.environ.get("GIT_TOKEN"))
-
 organization_name = "fuchicorp"
 
-
+## Getting github and organization 
+g = Github(os.environ.get("GIT_TOKEN"))
 organization = g.get_organization(organization_name)
+
+## Getting all members of organization 
 users = [ user.login for user in organization.get_members()]
 
+## Saving into organization-members.json file 
 with open("organization-members.json", 'w') as file:
     json.dump(users, file, indent=2)
--- github-management/manage-users/sync-users.py
+++ github-management/manage-users/sync-users.py
@@ -3,19 +3,30 @@ import os
 import argparse
 from github import Github
 
-## organization name and token on env
+## Organization name and token on env
 organization = 'fuchicorp'
-
 token = os.environ.get("GIT_ADMIN_TOKEN")
 
-g = Github('fsadykov', token, base_url='https://api.github.com')
-org = g.get_organization('fuchicorp')
 
+## Getting github user 
+g = Github(token, base_url='https://api.github.com')
+
+## Getting organization 
+org = g.get_organization(organization)
+
+## Getting all members of organization 
 users = [ user for user in org.get_members()]
 
+## Looping to each team in organization 
 for team in org.get_teams():
+
+    ## If team name is members 
     if 'members' == team.name.lower():
+
+        ## Looping to all memebers 
         for user in users:
+
+            ## Adding people to members team
             team.add_to_members(user)
             print(f"User <{user}> added to members")
         
